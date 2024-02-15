eeeae442310e46fd5f45c94f128df5734d40b7dd
Johnccg <150750774+Johnccg@users.noreply.github.com>
2024-02-15 12:54:45 -0600

    Laboratorio JavaScript

---
+++ intro_js.js
@@ -0,0 +1,8 @@
+//log, info, warn, error
+//assert si la condición es verdadera npo hace nada si es falsa manda un error
+//let para declarar variables
+
+//alert prompt confirm
+
+const nombre = prompt("¿Cómo te llamas?");
+alert("hola " + nombre);
