5257b6a5c27604ef34e900d6be6aa9abf8850efb
Johnccg <150750774+Johnccg@users.noreply.github.com>
2024-02-15 13:11:56 -0600

    Laboratorio de JS

--- intro_js.js
+++ intro_js.js
@@ -4,5 +4,42 @@
 
 //alert prompt confirm
 
-const nombre = prompt("¿Cómo te llamas?");
-alert("hola " + nombre);
+function saludar()
+{
+    const nombre = prompt("¿Cómo te llamas?");
+    alert("hola " + nombre);
+}
+
+saludar();
+
+let propmt = () => {
+    console.prompt("¿Verdad?");
+}
+
+//arreglos
+
+const arreglo = [1];
+arreglo.push(2);
+arreglo.push("3");
+
+arreglo["dos"] = 2;
+
+for (let valor of arreglo)
+{
+    console.log(valor);
+}
+
+for (let pos in arreglo)
+{
+    console.log(arreglo[pos]);
+}
+
+//objetos
+
+const pokemon = {tipo1: "Agua", tipo2: "Hada"};
+pokemon.nombre = "Primarina";
+console.log(pokemon);
+
+//Modificar html
+
+document.write("Esto es nuevo y proviene de Js");
