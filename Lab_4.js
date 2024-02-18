45a33d6deb10e448152cca32d1b3ff83c5105474
Johnccg <150750774+Johnccg@users.noreply.github.com>
2024-02-18 14:39:14 -0600

    "Avance de js"

--- Lab_4.js
+++ Lab_4.js
@@ -1,5 +1,18 @@
+const pruebas_contador = [
+    [-34, 157, -80, 12, 0, 43, -74, 379, 0, 45],
+    [-125, 56, 10, -178, 89, -200, 123, 45, -90, 177],
+    [190, -32, 55, -88, 12, 67, -199, 99, -45, 150],
+    [-57, 38, 145, -82, 103, -176, 64, -99, 21, 79],
+    [88, -150, 42, -20, 175, -110, 90, -67, 33, -199],
+    [-100, 123, -87, 66, -45, 189, -23, 78, -59, 134]
+]
+contador(pruebas_contador[0])
 
-const pruebas_inverso = [true, "34", 3487, -6259, -67, 42, -10, 184, 3355749, -342339470]
+for (let i of pruebas_contador){
+    contador(i)
+}
+
+const pruebas_inverso = [true, "34", 3487, -6259, -67, 42, -10, 184, 9345749, -342739470]
 
 for (let i of pruebas_inverso)
 {
@@ -53,8 +66,37 @@ function suma_aleatoria(){
     document.getElementById("Placeholder").innerHTML = text;
 }
 
-function contador(){
+function contador(arreglo){
+    let positivo = 0
+    let negativo = 0
+    let cero = 0
+    let text = "<p>Prueba de función Inverso:<br>Recibió: ["
 
+
+
+    for (let i in arreglo){
+        text += arreglo[i]
+        
+        if (i != arreglo.length - 1)
+        {
+            text += ", "
+        }
+
+        if (arreglo[i] < 0)
+        {
+            negativo++
+        }else if (arreglo[i] > 0){
+            positivo++
+        }else{
+            cero++
+        }
+    }
+
+    text +="]<br>Regresó: Hay " + negativo + " valores negativos, " + positivo + " valores positivos y " + cero + " ceros"
+
+    text +="</p>"
+
+    document.getElementById("funciones").innerHTML += text;
 }
 
 function promedio(){
@@ -62,7 +104,7 @@ function promedio(){
 }
 
 function inverso(num){
-    let text = "<p>Prueba de función Inverso:<br>Recibió: " + num + "<br>Regreso: "
+    let text = "<p>Prueba de función Inverso:<br>Recibió: " + num + "<br>Regresó: "
 
     if (typeof num == "number")
     {
@@ -100,7 +142,7 @@ function inverso(num){
 
     text +="</p>"
 
-    document.getElementById("Placeholder").innerHTML += text;
+    document.getElementById("funciones").innerHTML += text;
 }
 
 function problema(){
