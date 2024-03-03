bcacabfd04e10d0bd96fb35e6ecda8d83c26c7f3
Johnccg <150750774+Johnccg@users.noreply.github.com>
2024-03-02 20:06:56 -0600

    Reorganizar carpetas

--- Public/scripts/POE.js
+++ Public/scripts/POE.js
@@ -5,10 +5,10 @@ let counter_q = false
 boton_q.onclick = () => {
     if (counter_q)
     {
-        qiqi.src = "./imágenes/qiqi.png"
+        qiqi.src = "../public/imágenes/qiqi.png"
         counter_q = false
     }else{
-        qiqi.src = "./imágenes/qiqi2.png"
+        qiqi.src = "../public/imágenes/qiqi2.png"
         counter_q = true
     }
 }
